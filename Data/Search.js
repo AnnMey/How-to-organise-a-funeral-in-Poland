5969f6febc8642f9a0240262061d738fdd882ab1
MadCapTest <annmey@wp.pl>
2022-04-29 09:22:07 +0200

    TOC amended

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/00_Who_can_organise_a_burial/Closest_family_members.htm'],s:['00'],p:[['00',0],['occured',17]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/00_Who_can_organise_a_burial/Closest_family_members.htm'],s:['00'],p:[['00',0],['near',14]]});
