e618ff3a59ca2f21cc8543a5fbb6a9ff18ecd055
MadCapTest <annmey@wp.pl>
2022-06-30 21:50:57 +0200

    Amended and completed

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/00_Who_can_organise_a_burial/Closest_family_members.htm'],s:['00','polish'],p:[['00',0],['ceremony',26],['grant',60],['on',45],['the',0]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/00_Who_can_organise_a_burial/Closest_family_members.htm'],s:['00','place'],p:[['00',0],['cemeteries',56],['get',9],['of',52],['suit',27]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637880703058630173'
+    urlArgs: 't=637922221570064631'
 });
